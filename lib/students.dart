280077ac06879ca9447b72b7ecfa8e7f2a4bd648
Fariha-Ahsan <93832675+Fariha-Ahsan@users.noreply.github.com>
2021-11-22 13:09:04 +0600

    initial

---
+++ lib/students.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+class Country{
+  String name;
+  String continent;
+  String cases;
+  String deaths;
+  String image;
+  Country({
+    required this.cases,required this.name,required this.continent,required this.deaths,required this.image});
+
+
+}
